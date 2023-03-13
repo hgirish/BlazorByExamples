3caa160539ddb2a42aeb95fb92605ffeaaa7dee0
Girish <girish@post.com>
2023-03-12 22:17:44 -0700

    guess the number

---
+++ GuessTheNumber/GuessTheNumber/Shared/Game.razor.cs
@@ -0,0 +1,90 @@
+﻿using GuessTheNumber.Shared;
+using Microsoft.AspNetCore.Components;
+using System.Text;
+using System.Text.Json;
+
+namespace GuessTheNumber.Shared;
+
+public partial class Game
+{
+
+    [Inject]
+    ILogger<Game>? logger { get; set; } = default;
+
+    [Parameter]
+    public int? Digits { get; set; }
+
+    private int digitCount = 4;
+    private string answer = "";
+    private string guess = "";
+
+    private List<Row> guesses = new();
+    private bool winner = false;
+
+    protected override void OnParametersSet()
+    {
+        if (Digits.HasValue)
+        {
+            digitCount = (int)Digits.Value;
+        }
+        CalculateAnswer();
+    }
+
+    private void CalculateAnswer()
+    {
+        StringBuilder calculateAnswer = new StringBuilder();
+
+        for (int i = 0; i < digitCount; i++)
+        {
+            int nextDigit = new Random().Next(0, 10);
+            calculateAnswer.Append(nextDigit);
+        }
+        answer = calculateAnswer.ToString();
+        logger!.LogInformation($"The answer is {answer}");
+
+    }
+    private void GuessAnswer()
+    {
+        var curGuess = new Row
+        {
+            Guess = guess,
+            Matches = new string[digitCount]
+        };
+
+        for (int i = 0; i < digitCount; i++)
+        {
+            if (answer[i] == guess[i])
+            {
+                curGuess.Matches[i] = "match-pos";
+            }
+            else
+            {
+                if (answer.Contains(guess[i]))
+                {
+                    curGuess.Matches[i] = "match-val";
+                }
+            }
+        }
+        guesses.Add(curGuess);
+        guess = "";
+        if (guess == answer)
+        {
+            winner = true;
+        }
+
+        logger!.LogInformation(JsonSerializer.Serialize(guesses));
+    }
+
+    private void PlayAgain()
+    {
+        winner = false;
+        guesses = new();
+        CalculateAnswer();
+    }
+
+    public class Row
+    {
+        public string Guess { get; set; }
+        public string[] Matches { get; set; }
+    }
+}
